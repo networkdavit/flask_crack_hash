c06ffb7b3512f37ac8b10a24ba101e77eb881ce7
eobard <eobard@localhost.localdomain>
2021-08-10 01:31:21 +0400

    Adding new hash support and css

--- app.py
+++ app.py
@@ -4,9 +4,14 @@ import hashlib
 app = Flask(__name__)
 
 hashlib_md5 = hashlib.md5
+hashlib_sha1 = hashlib.sha1
+hashlib_sha224 = hashlib.sha224
 hashlib_sha256 = hashlib.sha256
 
+
 md5 = "5d41402abc4b2a76b9719d911017c592"
+sha1 = "aaf4c61ddcc5e8a2dabede0f3b482cd9aea9434d"
+sha224 = "ea09ae9cc6768c50fcee903ed054556e5bfc8347907f12598aa24193"
 sha256 = "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"
 
 
@@ -15,8 +20,11 @@ def identify_hash(hashed_value):
         sys.exit()
     if len(hashed_value) == len(md5):
         return hashlib_md5
+    elif len(hashed_value) == len(sha1):
+        return hashlib_sha1
+    elif len(hashed_value) == len(sha224):
+        return hashlib_sha224
     elif len(hashed_value) == len(sha256):
-        print("[+] SHA256")
         return hashlib_sha256
     else:
         return None
@@ -50,6 +58,10 @@ def check_hash():
             crack(identified_hash, hash_value)
             if identified_hash == hashlib_md5:
                 display_hash_type = "MD5"
+            elif identified_hash == hashlib.sha1:
+                display_hash_type = "SHA1"
+            elif identified_hash == hashlib.sha224:
+                display_hash_type = "SHA224"
             elif identified_hash == hashlib.sha256:
                 display_hash_type = "SHA256"
             return render_template("result.html", cracked_hash=word, hash_type=display_hash_type)
